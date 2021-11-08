74201e3ab3b1896e5a2bd09d33e3d933fe079da6
kagec <66302618+kagec@users.noreply.github.com>
2021-11-08 19:00:09 +0900

    `ImageUploader`を`App`コンポーネントに追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import ImageUploader from "./components/ImageUploader";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ImageUploader />
+    </div>
+  );
 };
 
 export default App;
