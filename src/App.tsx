f2518478ee07aa50558289f8ee47a80634c05150
kagec <66302618+kagec@users.noreply.github.com>
2021-11-12 18:40:07 +0900

    `AppContainer`スタイルコンポーネントを追加
    アプリが画面中央に表示されるようにする
    アプリ全体の`font-weight`と文字間隔を指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import styled from "styled-components";
 import ImageUploader from "./components/ImageUploader";
 
 const App = () => {
   return (
-    <div>
+    <AppContainer>
       <ImageUploader />
-    </div>
+    </AppContainer>
   );
 };
+
+const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+  letter-spacing: -0.035em;
+`;
 
 export default App;
