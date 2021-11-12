04101e159a0b0166cbfa224a6d95e90faef6d382
kagec <66302618+kagec@users.noreply.github.com>
2021-11-12 19:00:04 +0900

    `Header`スタイルコンポーネントを追加

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const ImageUploader: VFC = () => {
   return (
     <ImageUploaderConteiner>
-      <header>Upload your image</header>
+      <Header>Upload your image</Header>
       <div>FIle should be Jpeg Png...</div>
       <div>
         <div>Drag & Drop your image here</div>
@@ -23,6 +23,12 @@ const ImageUploaderConteiner = styled.div`
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
   border-radius: 12px;
   text-align: center;
+`;
+
+const Header = styled.header`
+  color: #4f4f4f;
+  font-size: 18px;
+  font-weight: 500;
 `;
 
 export default ImageUploader;
