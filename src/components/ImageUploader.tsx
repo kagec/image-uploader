bd8422b7acc362e44fc39f58e1b395f974e50d54
kagec <66302618+kagec@users.noreply.github.com>
2021-11-12 19:02:15 +0900

    `Hint`スタイルコンポーネントを追加

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -5,7 +5,7 @@ const ImageUploader: VFC = () => {
   return (
     <ImageUploaderConteiner>
       <Header>Upload your image</Header>
-      <div>FIle should be Jpeg Png...</div>
+      <Hint>FIle should be Jpeg Png...</Hint>
       <div>
         <div>Drag & Drop your image here</div>
       </div>
@@ -29,6 +29,12 @@ const Header = styled.header`
   color: #4f4f4f;
   font-size: 18px;
   font-weight: 500;
+`;
+
+const Hint = styled.div`
+  color: #828282;
+  font-size: 10px;
+  margin-top: 16px;
 `;
 
 export default ImageUploader;
