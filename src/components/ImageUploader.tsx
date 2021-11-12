ed981b35b638f4d83f375e888c36efd53ac0d35b
kagec <66302618+kagec@users.noreply.github.com>
2021-11-12 18:40:34 +0900

    `ImageUploader`の全体のレイアウトを調整

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,8 +1,9 @@
 import type { VFC } from "react";
+import styled from "styled-components";
 
 const ImageUploader: VFC = () => {
   return (
-    <div>
+    <ImageUploaderConteiner>
       <header>Upload your image</header>
       <div>FIle should be Jpeg Png...</div>
       <div>
@@ -13,8 +14,15 @@ const ImageUploader: VFC = () => {
         choose a file
         <input name="image" accept="image/*" type="file" />
       </label>
-    </div>
+    </ImageUploaderConteiner>
   );
 };
+
+const ImageUploaderConteiner = styled.div`
+  padding: 36px 32px;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  text-align: center;
+`;
 
 export default ImageUploader;
